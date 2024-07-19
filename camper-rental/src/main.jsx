a1e3395e6a0c43d56607243911a5ce65d303c6b8
Veronika <veronikapetrushka@gmail.com>
2024-07-19 13:40:19 +0200

    Update main.jsx

--- camper-rental/src/main.jsx
+++ camper-rental/src/main.jsx
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,11 +7,11 @@ import './index.css'
 // import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    // <Provider store={store}>
+  <React.StrictMode>
+    {/* // <Provider store={store}> */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    // </Provider>
-  // </React.StrictMode>,
+    {/* // </Provider> */}
+   </React.StrictMode>,
 )
