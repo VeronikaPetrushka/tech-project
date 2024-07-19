b3611d1823cc0ea504da0a71cd03410627796f75
Veronika <veronikapetrushka@gmail.com>
2024-07-19 14:11:33 +0200

    updated

--- camper-rental/src/App.jsx
+++ camper-rental/src/App.jsx
@@ -3,14 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-// const CataloguePage = lazy(() => import('./pages/CataloguePage/CataloguePage'));
+const CataloguePage = lazy(() => import('./pages/CataloguePage/CataloguePage'));
 
 const App = () => {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* <Route path="/catalogue" element={<CataloguePage />} /> */}
+      <Route path="/catalogue" element={<CataloguePage />} />
     </Routes>
   )
 }
