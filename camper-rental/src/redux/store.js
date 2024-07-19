d63a96a0c9d7fd8c014ada9b47935462b185e9d3
Veronika <veronikapetrushka@gmail.com>
2024-07-19 13:24:02 +0200

    commented part test

--- camper-rental/src/redux/store.js
+++ camper-rental/src/redux/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
-import campersReducer from "./slice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import campersReducer from "./slice";
 
-export const store = configureStore({
-  reducer: campersReducer,
-});
+// export const store = configureStore({
+//   reducer: campersReducer,
+// });
